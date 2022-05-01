871460248fe3d9573f9a4f845022f0f929658064
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-05-01 19:36:23 +0530

    Updated Init method with some logger info

--- src/main/java/com/softcare/jenkinsdemo/SpringJenkinsOneApplication.java
+++ src/main/java/com/softcare/jenkinsdemo/SpringJenkinsOneApplication.java
@@ -15,6 +15,7 @@ public class SpringJenkinsOneApplication {
 	@PostConstruct
 	public void init(){
 		logger.info("This is init method inside the main class");
+		logger.info("Checking whether it is working fine or not for scm");
 	}
 	public static void main(String[] args) {
 		logger.info("This is Main method inside the main class");
