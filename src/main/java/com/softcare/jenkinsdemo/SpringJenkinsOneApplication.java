b3d8169a8d7261d12d2cf8e431d8dc87ced03759
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-05-01 18:57:34 +0530

    initial commit of spring-jenkins-one project

---
+++ src/main/java/com/softcare/jenkinsdemo/SpringJenkinsOneApplication.java
@@ -0,0 +1,24 @@
+package com.softcare.jenkinsdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsOneApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsOneApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("This is init method inside the main class");
+	}
+	public static void main(String[] args) {
+		logger.info("This is Main method inside the main class");
+		SpringApplication.run(SpringJenkinsOneApplication.class, args);
+	}
+
+}
